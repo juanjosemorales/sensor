8f6ae21f0d6a9e02821728dc3f8aa9b708459168
juanjosemorales <juanjose.morales@gmail.com>
2014-12-28 13:47:43 -0600

    adding exception handler

--- arduino.py
+++ arduino.py
@@ -2,20 +2,24 @@ import serial
 import datetime
 import requests
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+
 url = "http://localsprout.pythonanywhere.com/monitoring/get_readings"
 i = 0
-while i == 0:
-	arduino_reading = str(arduino.readline()).strip()	
-	value = arduino_reading	
-	date = str(datetime.datetime.now())	
-	reading_type = ""
-	if "pH" in arduino_reading:
-		reading_type = "pH"
-	elif "EC" in arduino_reading:
-		reading_type = "EC"
-	else:
-		reading_type = "Bad"	
-	reading = { "type":reading_type, "date":date, "reading":value }
-	r = requests.post(url, data=reading) 
-	print(r.text)
+try:
+	arduino = serial.Serial('/dev/ttyACM0', 9600)
+	while i == 0:
+		arduino_reading = str(arduino.readline()).strip()	
+		value = arduino_reading	
+		date = str(datetime.datetime.now())	
+		reading_type = ""
+		if "pH" in arduino_reading:
+			reading_type = "pH"
+		elif "EC" in arduino_reading:
+			reading_type = "EC"
+		else:
+			reading_type = "Bad"	
+		reading = { "type":reading_type, "date":date, "reading":value }
+		r = requests.post(url, data=reading) 
+		print(r.text)
+except Exception, e:
+	print "Unexpected error: " + str(e)
